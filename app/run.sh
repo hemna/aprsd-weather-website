f82a2f8632c476fd6ee892d04384e9cd1825d0f9
Hemna <waboring@hemna.com>
2024-01-23 17:04:52 +0000

    update run.sh

--- app/run.sh
+++ app/run.sh
@@ -8,4 +8,5 @@ export COLUMNS
 #pip install whitenoise
 #pip freeze
 #/usr/local/bin/gunicorn -w 6 --conf gunicorn_conf.py --bind '[::]:80' "main:create_app(config_file='config/aprsd_repeat.conf')"
-gunicorn -w 6 --conf gunicorn_conf.py --bind '[::]:80' "main:create_app(config_file='config/aprsd_listen.conf')"
+#gunicorn -w 6 --conf gunicorn_conf.py --bind '[::]:80' "main:create_app(config_file='config/aprsd_listen.conf')"
+uvicorn --reload --factory "main:create_app"
