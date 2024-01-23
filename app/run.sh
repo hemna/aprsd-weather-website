025efeef1de97dda07c08fa9aa7d91365f40e9c7
Hemna <waboring@hemna.com>
2024-01-23 18:36:57 +0000

    more updates

--- app/run.sh
+++ app/run.sh
@@ -9,4 +9,4 @@ export COLUMNS
 #pip freeze
 #/usr/local/bin/gunicorn -w 6 --conf gunicorn_conf.py --bind '[::]:80' "main:create_app(config_file='config/aprsd_repeat.conf')"
 #gunicorn -w 6 --conf gunicorn_conf.py --bind '[::]:80' "main:create_app(config_file='config/aprsd_listen.conf')"
-uvicorn --reload --factory "main:create_app"
+uvicorn --reload --factory "main:create_app" --port 80 --host '0.0.0.0'
